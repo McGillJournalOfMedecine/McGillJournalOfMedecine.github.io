0a4cbab1cd0cf6a72d96b659581e7eefb195ea47
newphew92 <terrenceko@outlook.com>
2015-11-05 01:11:04 -0500

    Implemented all features that the front-end can handle.

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 var NEWS=[];
 var CAROUSEL=[];
+var STAFF=[];
 var today=new Date();
 var milliseconds=today.getMilliseconds();
 $(document).ready(function() {
-  $.ajaxSetup({ cache: false });
+	$.ajaxSetup({ cache: false });
 });
 $.ajaxSetup({
-      async: false,
-      cache: false
-  });
+			async: false,
+			cache: false
+	});
 //loads react components into the aray NEWS. The news page will display the contents of the array
 
 // $('body').css('background-image', 'Pictures/pen.png)');
-$.getJSON( "Data/news.json"+'?'+milliseconds, function() {
+$.getJSON( "Data/news.json"+'?'+milliseconds, { cache: false},function() {
 })
-  .done(function( data ) {
-      $.each( data, function( key, val ) {
-      	console.log(data)
-	       NEWS.push([<h1 id="title">{key}</h1>, <p>{val[2]}</p>, <i>Posted by {val[0]}</i>,<em> on {val[1]}</em>])
-      });
-    })
-  .fail(function() {
-    console.log( "error" );
-  })
-  .always(function() {
-  });
+	.done(function( data ) {
+			$.each( data, function( key, val ) {
+				 NEWS.push([<h1 id="title">{key}</h1>, <p>{val[2]}</p>, <i>Posted by {val[0]}</i>,<em> on {val[1]}</em>])
+			});
+		})
+	.fail(function() {
+		console.log( "error" );
+	})
+	.always(function() {
+	});
+
 
 
 var Home=React.createClass({
@@ -57,31 +58,61 @@ var Home=React.createClass({
 		// this.setState({})
 		
 		var B=ReactBootstrap,
-		CarouselItem=B.CarouselItem;
+		CarouselItem=B.CarouselItem,
+		Col=B.Col;
 		$.ajaxSetup({
-      async: false,
-      cache: false
-  	});
-		$.getJSON( "Data/pictureCarousel.json"+'?'+milliseconds, function() { 
+			async: false,
+			cache: false
+		});
+		$.getJSON( "Data/pictureCarousel.json"+'?'+milliseconds, {cache: false},function() {})
+		.done(function( data ) {
+			$.each( data, function( key, val ) {
+				var temp="../Pictures/"+val[0]
+				CAROUSEL.push([
+					<CarouselItem	key={key}>
+						<img style={{margin:"auto"}} width={450} height={600} alt="900x500" src={temp}/>
+						<div className="carousel-caption">
+							<h3>{key}</h3>
+							<p>{val[1]}</p>
+						</div>
+					</CarouselItem>])
+				});
 			})
-	  	.done(function( data ) {
-	      $.each( data, function( key, val ) {
-	      	var temp="../Pictures/"+val[0]
-	      	CAROUSEL.push([
-	      		<CarouselItem>
-				      <img style={{margin:"auto"}} width={450} height={600} alt="900x500" src={temp}/>
-				      <div className="carousel-caption">
-				        <h3>{key}</h3>
-				        <p>{val[1]}</p>
-				      </div>
-				    </CarouselItem>])
-	      });
-	    })
-	  .fail(function() {
-	    console.log( "error" );
-	  })
-	  .always(function() {
-	  });
+		.fail(function() {
+			console.log( "error" );
+		})
+		.always(function() {
+		});
+
+		$.getJSON( "Data/staff.json"+'?'+milliseconds, { cache: false},function() {})
+				.done(function( data ) {
+						$.each( data, function( key, val ) {
+
+
+							STAFF.push([
+								<Col key={key} lg={6} md={6} sm={12} xs={12} className="staffCol">
+									<h3 id="staffTitle">{key}</h3>
+									<ul>{val.map(function(el, i){return	<li key={i}>{el}</li>})}</ul>
+								</Col>])
+						});
+					})
+				.fail(function() {
+					console.log( "error" );
+				})
+				.always(function() {
+				});
+	},
+	organizeGroups: function(list, rowLength){
+		var b = [];var c = [];
+		for (var i = 1; i<= list.length;i++){
+			b=b.concat(list[i-1])
+			if(i%rowLength === 0||i === list.length){
+				c = c.concat([b]);  
+				b = [];
+			}
+		}
+		console.log(c)
+		return c;
 	},
 	render: function (){
 		var B=ReactBootstrap,
@@ -99,12 +130,12 @@ var Home=React.createClass({
 		NavDropdown=B.NavDropdown,
 		MenuItem=B.MenuItem,
 		Jumbotron=B.Jumbotron;
-
+		var staff=(this.organizeGroups(STAFF, 2)).map(function(el,i) {return <Row key={i} className="staffRow">{el}</Row>})
 		var screen={
 			main: (
-			  <Carousel>
-			    	{CAROUSEL}
-		    </Carousel>),
+				<Carousel>
+						{CAROUSEL}
+				</Carousel>),
 			news: (
 				<div>
 					<h1> Latest News </h1>
@@ -120,11 +151,34 @@ var Home=React.createClass({
 			archives: (
 				<div>
 					<h1> Archives </h1>
+					Not finished.
 				</div>
 				),
 			aboutUs: (
 				<div>
 					<h1> About Us </h1>
+					<p>The McGill Journal of Medicine (MJM) is a scholarly scientific journal providing an international forum for health sciences university students to publish a variety of literature and artwork. Established in May 1994, MJM is published and produced entirely by medical students and graduate students in the Faculty of Medicine at McGill and is driven by principles of student excellence and education. At the outset the MJM was the first student-run medical journal devoted to publish the original research of students exclusively, on an international scale.</p>
+					<p>This journal focuses on the disciplines of clinical medical, biomedical sciences as well as issues surrounding medical education and medical ethics. </p>
+					<h2>Executive Team</h2>
+					{staff}
+				</div>
+				),
+			latestIssue: (
+				<div>
+					<h1>Latest Issue</h1>
+					Not finished.
+				</div>
+				),
+			contactUs: (
+				<div>
+					<h1> Contact Us</h1>
+					Not finished.
+				</div>
+				),
+			represent: (
+				<div>
+					<h1>Represent the MJM at your university!</h1>
+					<p>The MJM’s editorial staff is comprised of students in the Faculty of Medicine at McGill University. Our goal is to publish high-quality work composed by students around the world. If you would like to represent the MJM at your university and help us promote opportunities for students to publish and facilitate discussion between students on the topic of medicine, join our volunteer staff today! </p>
 				</div>
 				),
 		};
@@ -144,26 +198,27 @@ var Home=React.createClass({
 								<img style={{width: "100px"}}id="pen" src="Pictures/pen.png"></img>
 							</Col>
 							<Col lg={11} md={12} xs={12} className="bottom-column">
-								<Row>	
+								<Row id ="headerText">	
 									<Col className="banner" lg={12} md={12} sm={12} xs={12}>
 										<h1 ><b id="headTitle">McGill Journal of Medecine</b></h1>
 									</Col>
 									<Col lg={12} md={12} xs={12}>	
 										<Navbar toggleNavKey={0} >
-									    <NavBrand><a href="#main" onClick = {this.handleScreen.bind(null,'main')}>Home</a></NavBrand>
-									    <Nav className="nana" eventKey={0}>
-									      <NavItem eventKey={1} href="#news" onClick = {this.handleScreen.bind(null,'news')}>News</NavItem>
-									      <NavItem eventKey={2} href="#submissions" onClick = {this.handleScreen.bind(null,'submissions')}>Submissions</NavItem>
-									      <NavDropdown eventKey={3} title="Archives" id="basic-nav-dropdown">
-									        <MenuItem eventKey="1" href="#archives" onClick = {this.handleScreen.bind(null,'archives')}>Old Stuff</MenuItem>
-									        <MenuItem eventKey="2">Really Old Stuff</MenuItem>
-									        <MenuItem eventKey="3">A Canadian discovered insulin</MenuItem>
-									        <MenuItem divider />
-									        <MenuItem eventKey="4">All</MenuItem>
-									      </NavDropdown>	
-									      <NavItem eventKey={2} href="#aboutUs" onClick = {this.handleScreen.bind(null,'aboutUs')}>About Us</NavItem>
-									    </Nav>
-									  </Navbar>
+											<NavBrand><a href="#main" onClick = {this.handleScreen.bind(null,'main')}>Home</a></NavBrand>
+											<Nav className="nana" eventKey={0}>
+												<NavItem eventKey={1} href="#latestIssue" onClick = {this.handleScreen.bind(null,'latestIssue')}>Latest Issue</NavItem>
+												<NavItem eventKey={2} href="#news" onClick = {this.handleScreen.bind(null,'news')}>News</NavItem>
+												<NavItem eventKey={3} href="#archives" onClick = {this.handleScreen.bind(null,'archives')}>Archives</NavItem>
+												<NavItem eventKey={4} href="#aboutUs" onClick = {this.handleScreen.bind(null,'aboutUs')}>About Us</NavItem>
+												<NavItem eventKey={5} href="#submissions" onClick = {this.handleScreen.bind(null,'submissions')}>Submissions</NavItem>
+												<NavDropdown eventKey={6} title="Contact" id="basic-nav-dropdown">
+													<MenuItem eventKey="1" href="#contactUs" onClick = {this.handleScreen.bind(null,'contactUs')}>Contact Us</MenuItem>
+													<MenuItem eventKey="2" href="#represent" onClick = {this.handleScreen.bind(null,'represent')}>Become a Representative</MenuItem>
+													<MenuItem divider />
+													<MenuItem eventKey="4">All</MenuItem>
+												</NavDropdown>	
+											</Nav>
+										</Navbar>
 									</Col>	
 								</Row>	
 							</Col>
